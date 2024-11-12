518ca1913205bb7835fa35dae9d78e516e6a4799
AlmostEvil665 <u1196904@utah.edu>
2024-11-11 19:24:20 -0700

    try again

--- utils/database.py
+++ utils/database.py
@@ -1014,8 +1014,8 @@ def add_wrapup_player_slayer_task(username, task_name, monsters_killed):
             VALUES (%s, 1, %s)
             ON CONFLICT (task_name)
             DO UPDATE SET
-                wrapup_slayer_tasks.tasks_completed = wrapup_slayer_tasks.tasks_completed + 1,
-                wrapup_slayer_tasks.monsters_killed = wrapup_slayer_tasks.monsters_killed + EXCLUDED.monsters_killed
+                tasks_completed = wrapup_slayer_tasks.tasks_completed + 1,
+                monsters_killed = wrapup_slayer_tasks.monsters_killed + EXCLUDED.monsters_killed
         """, (task_name, monsters_killed))
 
         # Commit the transaction to save changes
