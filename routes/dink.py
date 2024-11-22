3a16cc4afbcbd646624bfbbaeec47b555ce5e1c3
Danny Decker <u1196904@utah.edu>
2024-11-21 17:54:48 -0800

    fix decimal pb's

--- routes/dink.py
+++ routes/dink.py
@@ -2,6 +2,7 @@ import os
 import re
 from collections import defaultdict
 from datetime import timedelta
+from math import ceil
 
 from flask import Blueprint, jsonify, request
 import json
@@ -338,15 +339,18 @@ def parse_kill_count(data, img_file) -> dict[str, list[str]]:
 
     unparsed_time = data.get('extra').get('time')
     if unparsed_time:
-        match = re.match(r'PT(?:(\d+)H)?(?:(\d+)M)(\d+)S', unparsed_time)
+        match = re.match(r'PT(?:(\d+)H)?(?:(\d+)M)(\d+(\.\d+)?)S', unparsed_time)
         if match:
-            # Extract hours, minutes, and seconds
-            hours = int(match.group(1) or 0)  # Default to 0 if no hours
+            # Extract hours (default to 0 if not present), minutes, and seconds
+            hours = int(match.group(1) or 0)
             minutes = int(match.group(2))
-            seconds = int(match.group(3))
+            seconds = float(match.group(3))
+
+            # Round up the seconds to the nearest whole number
+            rounded_seconds = ceil(seconds)
 
             # Convert to total seconds
-            total_seconds = timedelta(hours=hours, minutes=minutes, seconds=seconds).total_seconds()
+            total_seconds = timedelta(hours=hours, minutes=minutes, seconds=rounded_seconds).total_seconds()
             add_wrapup_player_pb(rsn, boss_name, total_seconds)
         else:
             print(f"Invalid time format: {unparsed_time}")
